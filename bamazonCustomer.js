91500fca2777e0d5f0964134f0e74c3312d9bb34
Katherine Walters <kattymay222@gmail.com>
2019-08-28 20:11:40 -0400

    cleaner code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,109 +4,116 @@ var inquirer = require("inquirer");
 
 // Create the connection to sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "bamazon_db"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
 });
 
 // Connect to mysql server and sql database
 connection.connect(function (err) {
-  if (err) throw err;
-  displayItems();
+    if (err) throw err;
+    displayItems();
 });
 
-// Function to displayItems
-// Pull from sql table "products"
+// displayItems function will call on the connection to bamazon_db query
+// will select from products table
 function displayItems() {
-  connection.query("SELECT * FROM products", function (err, res) {
-    // If error, throw error
-    if (err) throw err;
-    // Console.table(response)
-    console.table(res);
-    // Call customerOrder
-    customerOrder();
-  });
+    connection.query("SELECT * FROM products",
+        function (err, res) {
+            // If error throw error
+            if (err) throw err;
+            // Console.table(response)
+            console.table(res);
+            // Invoke customerOrder
+            customerOrder();
+        });
 }
 
-// Function to prompt customerOrder
+// customerOrder function with inquirer prompt 
 function customerOrder() {
-  // Prompt user to input selected item and quantity
-  inquirer.prompt([{
-    name: "item_id",
-    message: "Enter item ID",
-    type: "input"
-  }, {
-    name: "quantity",
-    message: "How many?",
-    type: "input",
-    validate: function (value) {
-      if (isNaN(value) === false) {
-        return true;
-      }
-      return false;
-    }
-  }])
-// If user has submitted choice, match table with answers
-    .then(function (answer) {
-      var itemId = answer.item_id;
-      var itemQuantity = answer.quantity;
+    // Input prompt for user to enter item ID
+    inquirer.prompt([{
+        name: "item_id",
+        message: "Enter item ID",
+        type: "input"
+    }, {
+        // Input prompt for user to enter requested quantity 
+        name: "quantity",
+        message: "How many?",
+        type: "input",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }])
 
-// Pull from sql table "products" 
-      connection.query("SELECT * FROM  products WHERE  item_id =" + itemId, function (err, res) {
-        var selected = res[0];
-  
-// If not enough of item, display alert. Call displayItems
-        if (itemQuantity > selected.stock_quantity) {
-          console.log("Not enough in stock");
-          displayItems();
-        }
-        else {
-          // Place order if enough in stock
-          placeOrder(itemId, itemQuantity, selected.price);
-        }
-      });
-    }
-    )
+        // Use user input to update and match table
+        .then(function (answer) {
+            var itemId = answer.item_id;
+            var itemQuantity = answer.quantity;
+
+            // Pull from sql "products" table where item_id is equal to users selection 
+            // connection function will take arguments error and response
+            connection.query("SELECT * FROM products WHERE item_id =" +
+                itemId, function (err, res) {
+                    var selected = res[0];
+
+                    // ALert will display to user if there are not enough items in stock to match their order
+                    if (itemQuantity > selected.stock_quantity) {
+                        console.log("Not enough in stock");
+                        // Invoke displayItems
+                        displayItems();
+                    }
+                    else {
+                        // Else condition call to place order if enough in stock
+                        placeOrder(itemId, itemQuantity, selected.price);
+
+                    }
+                });
+        });
 }
-
-
-// placeOrder function
-// Update query with correct quantity of items
+// placeOrder function will call on "products" table
 function placeOrder(itemId, itemQuantity, price) {
-  connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id= ?", [itemQuantity, itemId], function (err, res) {
-    // If error, throw error
-    if (err) throw err;
+    // will update and set row stock_quantity after user makes a selection
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id= ?", [itemQuantity, itemId],
+        function (err, res) {
+            // will call on function to take error and response
+            if (err) throw err;
 
-    // Console.log to show order has been placed
-    // Console.log to show total price of order, include itemQuantity
-    console.log("Your order has been placed.");
-    console.log("The total cost of your purchase is $ " + (price * itemQuantity) + ".");
-    stillShopping();
-  })
+            // Console.log that order has been placed
+            console.log("Your order has been placed.");
+            // Console.log total cost of purchase
+            console.log("The total cost of your purchase is $ " + (price + itemQuantity) + ".");
+            stillShopping();
+        })
 }
-// Prompt function will display user choice to continue shopping
+// stillShopping function will push inquirer prompt for user to continue shopping or exit
+//  type: confirm 
 function stillShopping() {
-  inquirer.prompt([
-    {
-      name: "shop",
-      message: "Continue shopping?",
-      type: "confirm"
+    inquirer.prompt([{
+        name: "shop",
+        message: "Continue shopping?",
+        type: "confirm"
     }
-    // displayItems if user would like to continue
-  ]).then(function (answer) {
-    if (answer.shop) {
-      displayItems();
-    }
-    else {
-      // Console will exit if user is finished
-      console.log("Thank you for shopping!")
-      exit();
-    }
-  })
+    ])
+        // call on user's answer to invoke displayItems when user wishes to continue
+        .then(function (answer) {
+            if (answer.shop) {
+                displayItems();
+
+            }
+            // or else condition to console.log "Thank you" and exit shopping connection
+            else {
+                console.log("Thank you for shopping!")
+                exit();
+            }
+        })
 }
-// End connection 
+// End connection
 function exit() {
-  connection.end();
+    connection.end();
 }
